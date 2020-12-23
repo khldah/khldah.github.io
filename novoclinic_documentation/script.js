d31aecee3fbb2abb9256574d677a95f6a440014c
khldah <32120297+khldah@users.noreply.github.com>
2020-12-23 09:54:15 +0800

    Add files via upload

---
+++ novoclinic_documentation/script.js
@@ -0,0 +1,34 @@
+function mynav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+$('myTopnav a').click(function () {
+  $('#myTopnav a').removeClass('selected');
+  $(this).addClass('selected');
+});
+
+// dropdown sidebar navigation
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+document.querySelectorAll("code textarea").forEach(function (element) {
+  element.parentElement.innerHTML = element.innerHTML;
+
+});
